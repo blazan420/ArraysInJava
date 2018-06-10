63623059020f18af575198b7e45c156a9fb1fd86
kevin <kevinjobrien420@gmail.com>
2018-06-10 04:46:25 -0500

    173 App Number 12 - Part2 - Create a basic android app by using arrays in Java.. //Coding

--- app/src/main/java/com/example/kevin/arraysinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/arraysinjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.arraysinjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +11,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int[] integerNumbers = new int[100];
+        /*int[] integerNumbers = new int[100];
 
-        integerNumbers[0] =
+        integerNumbers[0] = 24;
+
+        Log.i("LOG" , integerNumbers[0] + "");
+        Log.i("LOG", integerNumbers[1] + "");
+
+        integerNumbers[1] = 50;
+
+        Log.i("LOG", "This is the number that is inside of the index 1 of intergetNumberArray " + integerNumbers[1]);*/
+
+        String[] arrayOfStrings = {"Animal", "Home", "Table"};
+
+        /*for (int index = 0; index < arrayOfStrings.length; index++) {
+
+            Log.i("LOG", "Index number: " + index + "The value that is inside of that index: " + index + " is: " +
+                                                               arrayOfStrings[index]);
+
+        }*/
+
+        Log.i("LOG", arrayOfStrings.length + "");
+
+
+        //Log.i("")
 
 
 
