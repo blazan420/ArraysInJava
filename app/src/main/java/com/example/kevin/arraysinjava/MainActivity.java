cea9fe6a770e89a555c44bba57f4c9e4d60244a5
kevin <kevinjobrien420@gmail.com>
2018-06-10 03:57:10 -0500

    172 App Number 12 - Part1 - Create a basic android app by using arrays in Java. //Coding

--- app/src/main/java/com/example/kevin/arraysinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/arraysinjava/MainActivity.java
@@ -9,5 +9,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] integerNumbers = new int[100];
+
+        integerNumbers[0] =
+
+
+
+
+
+
     }
 }
