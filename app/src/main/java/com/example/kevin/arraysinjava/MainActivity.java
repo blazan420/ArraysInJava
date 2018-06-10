c49964a91de4f0f8ad920f28cdd9472aeab4b185
kevin <kevinjobrien420@gmail.com>
2018-06-10 05:08:42 -0500

    174 App Number 12 - Part3 - Create a basic android app by using arrays in Java. ArraysInJava Complete

--- app/src/main/java/com/example/kevin/arraysinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/arraysinjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kevin.arraysinjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("LOG", "This is the number that is inside of the index 1 of intergetNumberArray " + integerNumbers[1]);*/
 
-        String[] arrayOfStrings = {"Animal", "Home", "Table"};
+        /*String[] arrayOfStrings = {"Animal", "Home", "Table"};
 
         /*for (int index = 0; index < arrayOfStrings.length; index++) {
 
@@ -31,10 +32,30 @@ public class MainActivity extends AppCompatActivity {
 
         }*/
 
-        Log.i("LOG", arrayOfStrings.length + "");
+        /*Log.i("LOG", arrayOfStrings.length + " ");
+
+        Log.i("LOG", arrayOfStrings[2]);*/
 
 
-        //Log.i("")
+        TextView txtIndex = (TextView)findViewById(R.id.txtIndex);
+        TextView txtValue = (TextView)findViewById(R.id.txtValue);
+        String oldTxtIndexValue;
+        String oldTxtValueValue;
+
+        int[] intArray = new int[10];
+
+        intArray[5] = 420;
+
+        for (int counter = 0; counter < intArray.length; counter++) {
+
+            oldTxtIndexValue = txtIndex.getText().toString();
+            oldTxtValueValue = txtValue.getText().toString();
+
+            txtIndex.setText(oldTxtIndexValue + counter + "\n");
+            txtValue.setText(oldTxtValueValue + intArray[counter] + "\n");
+        }
+
+
 
 
 
